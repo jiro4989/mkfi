c5d694787f5fa57e09825e512d6838a33a9bc4ab
jiro4989 <jiroron666@gmail.com>
2019-03-10 14:22:55 +0900

    :sparkles: 画像の左右反転処理をサブコマンドから実行できるようにした

--- subcmd/subcmd_flip.go
+++ subcmd/subcmd_flip.go
@@ -1,15 +1,34 @@
 package subcmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/jiro4989/mkfi/log"
+	"github.com/jiro4989/mkfi/usecase"
+	"github.com/spf13/cobra"
+)
 
 func init() {
 	RootCommand.AddCommand(flipCommand)
-	flipCommand.Flags().StringP("owner", "o", "", "owner of remote file")
+	flipCommand.Flags().StringP("outdir", "o", "out/trim", "Save dir")
 }
 
 var flipCommand = &cobra.Command{
 	Use:   "flip",
 	Short: "mkfi",
 	Long:  "mkfi",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Debug("start 'flip' subcommand.")
+
+		f := cmd.Flags()
+
+		log.Debug("get commandline option parameters.")
+		outDir, err := f.GetString("outdir")
+		if err != nil {
+			panic(err)
+		}
+		log.Debug("commandline options:outDir=", outDir)
+
+		targets := fetchTargetFiles(args)
+		usecase.FlipImageFiles(outDir, targets)
+		log.Debug("end 'flip' subcommand.")
+	},
 }
