c27eec6f947ea3293e9d2d9d5a4d8c67d1bc534b
jiro4989 <jiroron666@gmail.com>
2019-03-10 14:29:09 +0900

    :loud_sound: 画像入出力関数にデバッグログを追加

--- usecase/util.go
+++ usecase/util.go
@@ -4,22 +4,28 @@ import (
 	"image"
 	"image/png"
 	"os"
+
+	"github.com/jiro4989/mkfi/log"
 )
 
 func readImageFile(fn string) (img image.Image, err error) {
+	log.Debug("read image file. image=", fn)
 	w, err := os.Open(fn)
 	defer w.Close()
 	if err != nil {
 		return
 	}
+	log.Debug("read success.")
 	return png.Decode(w)
 }
 
 func writeImageFile(fn string, img image.Image) (err error) {
+	log.Debug("write image file. image=", fn)
 	w, err := os.Create(fn)
 	defer w.Close()
 	if err != nil {
 		return
 	}
+	log.Debug("write success.")
 	return png.Encode(w, img)
 }
